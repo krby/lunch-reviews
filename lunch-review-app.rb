61754077959334151d7f023c5bef8e4c6c6616b9
Jimmy <jiamingqiu951@gmail.com>
2015-04-22 22:09:28 +0000

    ability to add menu items added and working

--- lunch-review-app.rb
+++ lunch-review-app.rb
@@ -11,56 +11,87 @@ class User
 	include DataMapper::Resource
 	
 	property :id,           Serial
-	property :username,     String, required: true
 	property :email,        String, required: true, unique: true
-	property :school,       String
-	property :grade,        String
 	property :password,     BCryptHash
 	
-	has n, :Reviews
+	has n, :reviews
 end
 
-class Lunch # the actual lunch item of the day
+# the actual lunch item of the day
+class Lunch 
 	include DataMapper::Resource
 	
 	property :id,       Serial
-	property :name,     String, required: true
-	property :date,     DateTime, required: true # the date that the item is lunch
+	property :date,     Date, required: true # the date that the item is lunch
+	# property :name,     String, required: true
 	
-	has n, :MenuItem 
+	has n, :lunch_menu_items
+	has n, :menu_items, through: :lunch_menu_items
+end
+
+# Pairs lunch (date specific) and menu items
+# Allows for the many-many relationship
+class LunchMenuItem
+	include DataMapper::Resource
+
+	property :id, Serial
 	
+	belongs_to :menu_item
+	belongs_to :lunch
 end
 
 class MenuItem 
 	include DataMapper::Resource
 	
-	property :id,       Serial
-	property :name,     String, required: true
-	property :title,    Text, required: true
-	property :body,     Text, required: true
-	
-	has n, :Review
+	property :id,								Serial
+	property :name,							String, required: true # name of the menu item
+	property :description,			Text, required: true
 
+	
+	has n, :review
+	has n, :lunch_menu_items
+	has n, :lunches, through: :lunch_menu_items
 end
+
+
 
 class Review
 	include DataMapper::Resource
 	
 	property :id,               Serial 
-	property :title,            Text
 	property :body,             Text, required: true
 	property :created_at,       DateTime, required: true
 	property :likes,            Integer, default: 0
+	property :stars,            Integer, required: true
+	property :summary,          String
 	
-	# rating??? should it be like this???
-	property :stars,            Integer, default: 0
-	
-	belongs_to :user, :Lunch
+	belongs_to :user, :menu_item
 end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
 
 get("/") do
   erb(:index)
 end
 
-	
+get("/add_menu_items") do
+	list = MenuItem.all(order: :name.asc)
+	erb(:add_menu_items, locals: { menu_items: list })
+end
 
+post("/add_menu_item") do
+  menu_item_name = params["name"]
+  menu_item_desc = params["description"]
+  
+  menu_item = MenuItem.create(
+    name: 							menu_item_name,
+    description: 				menu_item_desc
+  )
+ 
+  if menu_item.saved?
+    redirect("/")
+  else
+		erb(:add_menu_items, locals: { menu_items: list })
+  end
+end
